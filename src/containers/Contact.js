33a932335ff0b585b210715615367e91495e367f
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-14 19:04:00 -0400

    contact containers working

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ContactBody from '../components/ContactBody'
 
 class Contact extends React.Component {
 
 	render = () => {
 		return (
 			<div>
-				<h1 className="PageTitle"> Contact Me</h1>
+				<h1 className="PageTitle"> Connect With Me</h1>
+				<ContactBody />
 			</div>
 		)
 	}
