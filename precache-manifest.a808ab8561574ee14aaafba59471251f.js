317cae43b007c924fa4cf8faf086870300869b91
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-15 23:04:37 -0400

    Updates

---
+++ precache-manifest.a808ab8561574ee14aaafba59471251f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a44f8ba4b9bbdbbd05ac19078c85c05",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf101115520374682146",
+    "url": "/portfolio/static/css/main.3738c02c.chunk.css"
+  },
+  {
+    "revision": "343255697f799015b1fd",
+    "url": "/portfolio/static/js/2.efaf5eed.chunk.js"
+  },
+  {
+    "revision": "cf101115520374682146",
+    "url": "/portfolio/static/js/main.5b85c7ab.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8edaebee0e68b6dee03757c5c6243114.js"
+  "/portfolio/precache-manifest.a808ab8561574ee14aaafba59471251f.js"
 );
 
 self.addEventListener('message', (event) => {
