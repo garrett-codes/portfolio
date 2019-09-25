d55603f9404c088e9bc87d470f8f40a2f98db99c
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-25 16:50:36 -0400

    added more to home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import Typed from 'react-typed';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
 
 	render = () => {
 		return (
-			<div className="typewriter"> 
-				<Typed className="typewriter" style={{color: '#FFFFFF', fontSize: 64}}
-	        strings={[
-	          'Hello, welcome to my page.',
-	          'Please explore my projects',
-	          'Please explore my accomplishments.']}
-	          typeSpeed={45}
-	          backSpeed={30}
-						>
-	      </Typed>
+			<div className="home-container">
+				<div className="background-container">
+					<div className="home-name">
+						<Link to="/about"><h1 className="my-name">Garrett Halstein</h1></Link>
+					</div>
+					<img className="home-background" src={require(`../images/home-background.jpeg`)} />
+				</div>
+				<div className="typewriter"> 
+					<Typed className="typewriter" style={{color: '#FFFFFF', fontSize: 64}}
+		        strings={[
+		          'Hello, welcome to my page.',
+		          'Please explore my projects',
+		          'Please explore my accomplishments.']}
+		          typeSpeed={45}
+		          backSpeed={30}
+							>
+		      </Typed>
+				</div>
 			</div>
 		)
 	}
