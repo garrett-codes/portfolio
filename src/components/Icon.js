61d3649d224a5d0913bee5a896f0808150faf549
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-25 19:20:07 -0400

    added more to icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -5,7 +5,7 @@ class Icon extends React.Component {
 	render = () => {
 		return (
 			<Tilt className="tech-icon-tilt">
-				<img className="tech-icon" src={this.props.link} />
+				<img className={`tech-icon ${this.props.type}`} src={this.props.link} />
 			</Tilt>
 		)
 	}
